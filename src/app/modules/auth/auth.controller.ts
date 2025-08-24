3d0b11ad386e8a7392ecbe0c9cc1592e9c36373b
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-08-24 12:22:43 +0600

    service added to auth controller

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,35 +1,44 @@
 import type { RequestHandler } from "express";
+import authService from "./auth.service.js";
 
 const register: RequestHandler = async (req, res) => {
-  console.log(req.body);
+  const result = await authService.register(req.body);
+  res.status(201).json(result);
 };
 
 const login: RequestHandler = async (req, res) => {
-  console.log(req.body);
+  const result = await authService.login(req.body);
+  res.status(200).json(result);
 };
 
 const logout: RequestHandler = async (req, res) => {
-  console.log("logout");
+  const result = await authService.logout();
+  res.status(200).json(result);
 };
 
 const refresh: RequestHandler = async (req, res) => {
-  console.log("refresh");
+  const result = await authService.refresh();
+  res.status(200).json(result);
 };
 
 const forgotPassword: RequestHandler = async (req, res) => {
-  console.log(req.body);
+  const result = await authService.forgotPassword(req.body);
+  res.status(200).json(result);
 };
 
 const resetPassword: RequestHandler = async (req, res) => {
-  console.log(req.body);
+  const result = await authService.resetPassword(req.body);
+  res.status(200).json(result);
 };
 
 const verifyEmail: RequestHandler = async (req, res) => {
-  console.log(req.body);
+  const result = await authService.verifyEmail(req.body);
+  res.status(200).json(result);
 };
 
 const resendVerificationEmail: RequestHandler = async (req, res) => {
-  console.log(req.body);
+  const result = await authService.resendVerificationEmail(req.body);
+  res.status(200).json(result);
 };
 
 const authController = {
