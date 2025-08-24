2a4bc304f371f90ae42b94d75a1a89399380a957
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-08-24 12:17:21 +0600

    auth service template

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const register = async (payload: any) => {
+  console.log(payload);
+};
+
+const login = async (payload: any) => {
+  console.log(payload);
+};
+
+const logout = async () => {
+  console.log('logout');
+};
+
+const refresh = async () => {
+  console.log('refresh');
+};
+
+const forgotPassword = async (payload: any) => {
+  console.log(payload);
+};
+
+const resetPassword = async (payload: any) => {
+  console.log(payload);
+};
+
+const verifyEmail = async (payload: any) => {
+  console.log(payload);
+};
+
+const resendVerificationEmail = async (payload: any) => {
+  console.log(payload);
+};
+
+const authService = {
+  register,
+  login,
+  logout,
+  refresh,
+  forgotPassword,
+  resetPassword,
+  verifyEmail,
+  resendVerificationEmail,
+};
+export default authService;
