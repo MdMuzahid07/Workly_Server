aa2a641daa52bf0c441f582ef6151dd7fb78c6cf
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-08-26 21:35:54 +0600

    working on profile

--- src/app/modules/profile/profile.sevice.ts
+++ src/app/modules/profile/profile.sevice.ts
@@ -1,9 +1,9 @@
 import httpStatus from "http-status";
-import type { Profile } from "../../../generated/prisma/index.js";
+import type { Profile, Skill } from "../../../generated/prisma/index.js";
 import prisma from "../../../utils/prismaClient.js";
 import AppError from "../../error/AppError.js";
 
-const createProfile = async (payload: Profile) => {
+const createProfile = async (payload: Profile & { skills?: Skill[] }) => {
   const isUserExits = await prisma.user.findUnique({
     where: {
       id: payload.userId,
@@ -14,27 +14,46 @@ const createProfile = async (payload: Profile) => {
     throw new AppError(httpStatus.BAD_REQUEST, `User not found to create profile`);
   }
 
-  const profile = await prisma.profile.create({
-    data: {
-      userId: payload.userId,
-      bio: payload.bio,
-      location: payload.location,
-      avatarUrl: payload.avatarUrl,
-      coverUrl: payload.coverUrl,
-      resumeUrl: payload.resumeUrl,
-      linkedInUrl: payload.linkedInUrl,
-      websiteUrl: payload.websiteUrl,
-    },
+  const result = await prisma.$transaction(async (transactor) => {
+    const userProfile = await transactor.profile.create({
+      data: {
+        userId: payload.userId,
+        bio: payload.bio,
+        location: payload.location,
+        avatarUrl: payload.avatarUrl,
+        coverUrl: payload.coverUrl,
+        resumeUrl: payload.resumeUrl,
+        linkedInUrl: payload.linkedInUrl,
+        websiteUrl: payload.websiteUrl,
+      },
+    });
+
+    if (!userProfile) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Failed to create profile");
+    }
+
+    if (payload.skills?.length) {
+      const skillSet = await transactor.skill.createMany({
+        data: payload.skills?.map((skill: Skill) => ({
+          skillName: skill.skillName,
+          profileId: userProfile.id,
+          experienceYears: skill.experienceYears,
+        })),
+      });
+
+      if (!skillSet) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Failed to create profile");
+      }
+    }
+
+    return userProfile;
   });
 
-  if (!profile) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Failed to create profile");
-  }
-
-  return profile;
+  return result;
 };
 
 const profileService = {
   createProfile,
 };
+
 export default profileService;
