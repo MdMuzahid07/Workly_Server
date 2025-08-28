ce4a2abff63892d6cf5b8656699e25a456fc993f
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-08-29 00:33:35 +0600

    create company functional

--- src/app/modules/company/company.route.ts
+++ src/app/modules/company/company.route.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import { UserRole } from "../../../generated/prisma/index.js";
+import authValidator from "../../middleware/authValidator.js";
+import requestValidator from "../../middleware/requestValidator.js";
 import companyController from "./company.controller.js";
+import companyValidation from "./company.validation.js";
 
 const router = express.Router();
 
 router.post(
   "/new-company",
-  //  requestValidator(),
+  authValidator(UserRole.EMPLOYER),
+  requestValidator(companyValidation.createCompany),
   companyController.createCompany,
 );
 
